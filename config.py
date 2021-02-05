d95f1297d09f41f80c8795a29db5c5c0fed7e843
kenanyaman <keykubat_1@hotmail.com>
2021-02-06 01:07:00 +0300

    setting

---
+++ config.py
@@ -0,0 +1,7 @@
+################Telegram Setting############################
+bot_token = "693635518:AAHVocxc0_KT2h1Q_SuF1WubpnwD_CcUHBk"
+chat_id = "493551325"
+
+################Timer Setting########################
+time_hour = 0
+time_minute = 5
